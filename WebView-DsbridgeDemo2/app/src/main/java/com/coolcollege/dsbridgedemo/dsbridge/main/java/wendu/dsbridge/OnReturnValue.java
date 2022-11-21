ad26e8ea06cf19efbea5e4f39565c9a77971a6af
dong_shuai_wen <dongshuaiwenchina@163.com>
2022-11-21 18:54:15 +0800

    手动导入dsbridge库(并且将原tbs v3.5.0换成客户工程中的v4.3.0)

---
+++ WebView-DsbridgeDemo2/app/src/main/java/com/coolcollege/dsbridgedemo/dsbridge/main/java/wendu/dsbridge/OnReturnValue.java
@@ -0,0 +1,9 @@
+package com.coolcollege.dsbridgedemo.dsbridge.main.java.wendu.dsbridge;
+
+/**
+ * Created by du on 16/12/31.
+ */
+
+public interface OnReturnValue<T> {
+    void onValue( T retValue);
+}
